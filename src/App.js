45d21e217009c50821e956a0ca2f87ce00a66a6c
ebrar <ebrkdmr1@gmail.com>
2023-12-19 21:32:54 +0300

    sass edits

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Projects from "./pages/projects/Projects";
+import Contact from "./pages/contact/Contact";
+import NotFound from "./components/Error/NotFound";
+import { Routes, Route } from "react-router-dom";
+import "./scss/main.scss";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+        <Projects />
+        <Contact />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
